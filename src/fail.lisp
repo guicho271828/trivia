62c6034ce2338cca4489346d0429be661b27d4dc
Tomohiro Matsuyama <tomo@cx4a.org>
2012-11-10 00:08:27 +0900

    Optimize failure code

--- src/fail.lisp
+++ src/fail.lisp
@@ -3,15 +3,19 @@
 (defmacro try (form else)
   "Tries to evaluate FORM. If a form (FAIL) found in FORM, then go to
 ELSE branch for evaluation."
-  (let ((block (gensym "MATCH-TRY"))
-        (tag (gensym "MATCH-FAIL")))
-    `(block ,block
-       (tagbody
-          (return-from ,block
-            (macrolet ((fail () `(go ,',tag)))
-              ,form))
-          ,tag
-          (return-from ,block ,else)))))
+  (if (equal else '(fail))
+      ;; Pass through.
+      form
+      ;; Introduce try-fail block.
+      (let ((block (gensym "MATCH-TRY"))
+            (tag (gensym "MATCH-FAIL")))
+        `(block ,block
+           (tagbody
+              (return-from ,block
+                (macrolet ((fail () `(go ,',tag)))
+                  ,form))
+              ,tag
+              (return-from ,block ,else))))))
 
 (defmacro iff (test then else)
   "Similar to IF except IFF also allows to go to ELSE branch from THEN
