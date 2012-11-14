5e05ba39adb2f27e18eeb467c41d46a255459ec1
Tomohiro Matsuyama <tomo@cx4a.org>
2012-11-15 00:20:26 +0900

    FAIL can now take an optional argument
    If the argument is true, the rest of all pattern matching are regarded
    as failed.

--- src/fail.lisp
+++ src/fail.lisp
@@ -7,23 +7,33 @@ to jump to its next branch."
   (cond ((null forms) '(fail))
         ((null (rest forms)) (first forms))
         (t
-         (let ((block (gensym "BLOCK")))
+         (let* ((block (gensym "BLOCK"))
+                (else (car (last forms)))
+                (forms (butlast forms))
+                (tags (make-gensym-list (length forms) "TAG"))
+                (else-tag (car (last tags))))
            `(block ,block
               (tagbody
-                 ,@(loop for form in (butlast forms)
-                         for tag = (gensym "FAIL")
-                         collect `(return-from ,block
-                                    (macrolet ((fail () `(go ,',tag)))
-                                      ,form))
+                 ,@(loop for form in forms
+                         for tag in tags
+                         collect
+                         `(return-from ,block
+                            (macrolet ((fail (&optional all)
+                                         (if all
+                                             `(go ,',else-tag)
+                                             `(go ,',tag))))
+                              ,form))
                          collect tag)
-                 (return-from ,block ,(car (last forms)))))))))
+                 (return-from ,block ,else)))))))
 
 (defmacro %if (test then else)
   "Similar to IF except %IF also allows to call (FAIL) in THEN branch
 to jump to ELSE branch."
   `(%or (if ,test ,then (fail)) ,else))
 
-(defmacro fail ()
-  "Causes the latest pattern matching be failed and continue to do the
-rest of pattern matching."
+(defmacro fail (&optional all)
+  "If ALL is true, this causes the all pattern matching be failed. If
+ALL is false, this causes the latest pattern matching be failed and
+continue to do the rest of pattern matching."
+  (declare (ignore all))
   (error "Not pattern matching."))
