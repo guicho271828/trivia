8b54939a490807370495f477ba3e5cbfd5c12d1a
Tomohiro Matsuyama <tomo@cx4a.org>
2013-07-30 18:27:53 +0900

    Workaround for using together with ITERATE.

--- src/fail.lisp
+++ src/fail.lisp
@@ -16,8 +16,14 @@ to jump to its next branch."
               (tagbody
                  ,@(loop for form in (butlast forms)
                          for tag = (gensym "FAIL")
+                         ;; FIXME: ITER doesn't support MACROLET in
+                         ;; iteration body.  So we use SYMBOL-MACROLET
+                         ;; instead.
+                         ;collect `(return-from ,block
+                         ;           (macrolet ((fail () `(go ,',tag)))
+                         ;             ,form))
                          collect `(return-from ,block
-                                    (macrolet ((fail () `(go ,',tag)))
+                                    (symbol-macrolet ((%fail (go ,tag)))
                                       ,form))
                          collect tag)
                  (return-from ,block ,(car (last forms)))))))))
@@ -30,4 +36,9 @@ to jump to ELSE branch."
 (defmacro fail ()
   "Causes the latest pattern matching be failed and continue to do the
 rest of pattern matching."
-  (error "Not pattern matching."))
+  ;; FIXME: Don't raise an error but expand to %FAIL for the hack
+  ;; above.
+  ;(error "Not pattern matching.")
+  '%fail)
+
+(define-symbol-macro %fail (error "Not pattern matching."))
