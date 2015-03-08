290459a7d81aba084a0924f873595739bfa7d175
Masataro Asai <guicho2.71828@gmail.com>
2015-03-08 17:50:56 +0900

    ppcre: soft check only under a mismatch in the # of patterns

--- ppcre/package.lisp
+++ ppcre/package.lisp
@@ -16,15 +16,24 @@
   (destructuring-bind (regexp &key start end sharedp)
       (ensure-list regexp-and-options)
     (let ((register-num (count :register (flatten (parse-string regexp)))))
-      (unless (= (length subpatterns) register-num)
-        (simple-style-warning
-         "The number of registers ~a in ~a is different than the # of subpatterns ~a in ~a!"
-         register-num regexp (length subpatterns) subpatterns))
-      (with-gensyms (it)
-        `(guard1 ,it (stringp ,it)
-                 (nth-value 1 (scan-to-strings
-                               ,regexp ,it
-                               ,@(when start `(:start ,start))
-                               ,@(when end `(:end ,end))
-                               ,@(when sharedp `(:sharedp ,sharedp))))
-                 (simple-vector* ,@subpatterns))))))
+      (if (= (length subpatterns) register-num)
+          (with-gensyms (it)
+            `(guard1 ,it (stringp ,it)
+                     (nth-value 1 (scan-to-strings
+                                   ,regexp ,it
+                                   ,@(when start `(:start ,start))
+                                   ,@(when end `(:end ,end))
+                                   ,@(when sharedp `(:sharedp ,sharedp))))
+                     (simple-vector ,@subpatterns)))
+          (with-gensyms (it)
+            (simple-style-warning
+             "The number of registers ~a in ~a is different than the # of subpatterns ~a in ~a!"
+             register-num regexp (length subpatterns) subpatterns)
+            `(guard1 ,it (stringp ,it)
+                     (nth-value 1 (scan-to-strings
+                                   ,regexp ,it
+                                   ,@(when start `(:start ,start))
+                                   ,@(when end `(:end ,end))
+                                   ,@(when sharedp `(:sharedp ,sharedp))))
+                     ;; soft vector length check
+                     (simple-vector* ,@subpatterns)))))))
