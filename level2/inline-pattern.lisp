ef75f4ed37ca9078a29599aa305cd43e43a91a69
Masataro Asai <guicho2.71828@gmail.com>
2015-12-04 03:27:24 +0900

    @@

--- level2/inline-pattern.lisp
+++ level2/inline-pattern.lisp
@@ -13,3 +13,13 @@
 ;;              (format s "Toplevel inline pattern is invalid: ~a" (patterns c)))))
 
 ;; (cerror 'inline-pattern :patterns patterns)
+
+
+(defpattern-inline @@ (n &rest patterns)
+    "inline N successive identical patterns"
+  (assert (integerp n))
+  (mappend (lambda (i)
+             (declare (ignore i))
+             (copy-list patterns))
+           (iota n)))
+
