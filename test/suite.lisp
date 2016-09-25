4f2da031da640ee9ccc0864db7f361df47af767c
Masataro Asai <guicho2.71828@gmail.com>
2016-09-25 21:42:37 +0100

    it works only for inlinable function; slot accessors are such instances.

--- test/suite.lisp
+++ test/suite.lisp
@@ -55,8 +55,10 @@
     (make-load-form-saving-slots o
                                  :slot-names '(x y z)
                                  :environment environment))
+  (declaim (inline x))
   (defun x (a b c)
-    (format t "this function is meaningless ~a ~a ~a" a b c)))
+    (format t "this function is meaningless ~a ~a ~a" a b c))
+  (declaim (notinline x)))
 
 (test (constant-pattern :compile-at :definition-time)
   ;; integer
