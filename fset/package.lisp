d93d35de9e451c940c179a79d8b6e508e7c53af0
Manfred Bergmann <mb@software-by-mabe.com>
2021-06-01 19:58:10 +0200

    changed pattern names to match fset functions. changed fset:map pattern to match the constructor of map.

--- fset/package.lisp
+++ fset/package.lisp
@@ -1,10 +1,6 @@
 (defpackage :trivia.fset
   (:use :cl)
-  (:export #:fset-equal
-           #:fset-map
-           #:fset-set
-           #:fset-seq
-           #:fset-seq*))
+  (:export #:fset-seq*))
 
 (defpackage :trivia.fset.impl
   (:use :cl :trivia.level0 :trivia.level1 :trivia.level2
@@ -14,18 +10,19 @@
 
 (in-package :trivia.fset.impl)
 
-(defpattern fset-equal (arg)
+(defpattern fset:equal? (arg)
   "Matches for equality of fset collections. See `fset:equal?`."
   (with-gensyms (it)
     `(guard1 (,it :type ,(type-of arg))
              (fset:equal? ,it ,arg))))
 
-(defpattern fset-map (&rest args)
+(defpattern fset:map (&rest args)
   "Matches on existance of one or multiple keys in the fset:map collection. Capturing also works."
+  (print args)
   `(and ,@(mapcar (lambda-match0
                     ((cons key pattern)
                      `(fset-map-key ,key ,pattern)))
-                  (plist-alist args))))
+                  (mapcar (lambda (l) `(,(car l) . ,(cadr l))) args))))
 
 (defpattern fset-map-key (key subpattern &optional (default nil) (foundp nil foundp-suppliedp))
   (with-gensyms (it value indicator)
@@ -39,7 +36,7 @@
 (defun fset-lookup (collection key default)
   (or (fset:lookup collection key) default))
 
-(defpattern fset-set (&rest args)
+(defpattern fset:set (&rest args)
   "Matches on existance of one or multiple elements in the given set. fset:set is unordered."
   `(and ,@(mapcar (lambda-match0
                     (element
@@ -51,14 +48,14 @@
     `(guard1 (,it :type fset:set)
              (fset:contains? ,it ,element))))
 
-(defpattern fset-seq (&rest args)
+(defpattern fset:seq (&rest args)
   "Matches against the same size of elements in fset:seq."
   (if args
-      `(fset-cons ,(car args) (fset-seq ,@(cdr args)))
+      `(fset-cons ,(car args) (fset:seq ,@(cdr args)))
       `(fset-null)))
 
 (defpattern fset-seq* (&rest args)
-  "Matches against `fset:seq. The sizes of patterns and fset:seq size does not need to match.
+  "Matches against fset:seq. The sizes of patterns and fset:seq size does not need to match.
 The last subpattern is destructured against the tail (or a subseq) of fset:seq."
   (if (cdr args)
       `(fset-cons ,(car args) (fset-seq* ,@(cdr args)))
