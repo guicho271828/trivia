5305afa33498b587ca94a4b410bc0899e2ca836f
Masataro Asai <guicho2.71828@gmail.com>
2021-09-15 13:48:52 -0400

    disable declaration-information on clisp

--- level2/impl.lisp
+++ level2/impl.lisp
@@ -375,7 +375,7 @@ or results in a compilation error when this is the outermost matching construct.
          (clauses (mapcar (curry #'pad (length whats)) clauses)) ; adjust the length of the clauses
          (clauses (mapcar #'expand-clause clauses))
          (clauses* (if args
-                       (funcall (or #-(or abcl clasp ecl mezzano)
+                       (funcall (or #-(or abcl clasp ecl mezzano clisp)
                                     (when-let ((it (trivial-cltl2:declaration-information 'optimizer env)))
                                       (symbol-optimizer it))
                                     (symbol-optimizer *optimizer*))
