9e38bf25c46f44c31fde689cfee09cc0aaf36176
Chun Tian (binghe) <binghe.lisp@gmail.com>
2023-03-01 12:03:54 +0100

    FTBFS LispWorks old versions (5.x, 6.x, 7.0)

--- level2/impl.lisp
+++ level2/impl.lisp
@@ -361,7 +361,8 @@ or results in a compilation error when this is the outermost matching construct.
 ;; the effect should last after the compilation/load
 (proclaim '(declaration optimizer))
 
-#-(or abcl clasp ecl mezzano clisp)
+#-(or abcl clasp ecl mezzano clisp
+      lispworks5 lispworks6 lispworks7.0) ; hcl:define-declaration was added since LW 7.1
 (trivial-cltl2:define-declaration optimizer (specifier env)
   (declare (ignorable env))
   ;; (print specifier)
@@ -375,7 +376,8 @@ or results in a compilation error when this is the outermost matching construct.
          (clauses (mapcar (curry #'pad (length whats)) clauses)) ; adjust the length of the clauses
          (clauses (mapcar #'expand-clause clauses))
          (clauses* (if args
-                       (funcall (or #-(or abcl clasp ecl mezzano clisp)
+                       (funcall (or #-(or abcl clasp ecl mezzano clisp
+                                          lispworks5 lispworks6 lispworks7.0)
                                     (when-let ((it (trivial-cltl2:declaration-information 'optimizer env)))
                                       (symbol-optimizer it))
                                     (symbol-optimizer *optimizer*))
